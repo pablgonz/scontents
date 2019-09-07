2790ef6dda98838ec25712929b6f43a449a9de4b
Pablo González L <pablgonz@yahoo.com>
2019-09-07 12:19:12 -0400

    Added tag at build.lua

--- build.lua
+++ build.lua
@@ -1,13 +1,37 @@
 -- Build script for scontents
-pkgversion   = "1.2"
-pkgdate      = "2019/08/28"
+pkgversion   = "1.2e"
+pkgdate      = "2019/09/07"
 
 module       = "scontents"
 ctanpkg      = "scontents"
 ctanzip      = ctanpkg.."-"..pkgversion
 
-tagfiles     = {"sources/scontents.md","sources/scontents.sty","sources/scontents.dtx"}
+tagfiles     = {"sources/README.md","sources/scontents.sty","sources/scontents.dtx"}
 
+function update_tag (file,content,tagname,tagdate)
+ tagdate = string.gsub (pkgdate,"-", "/")
+ if string.match (file, "scontents.sty" ) then
+  content = string.gsub (content,  
+                         "\\ProvidesExplPackage{(.-)}{.-}{.-}",
+                         "\\ProvidesExplPackage{%1}{".. tagdate.."}{"..pkgversion .."}")
+  return content                         
+ elseif string.match (file, "scontents.dtx") then
+  content = string.gsub (content,  
+                         "\\ProvidesExplPackage{(.-)}{.-}{.-}",
+                         "\\ProvidesExplPackage{%1}{".. tagdate.."}{"..pkgversion .. "}")
+  return content 
+ elseif string.match (file, "README.md") then
+   content = string.gsub (content,  
+                         "Version: %d%.%d.%s-",
+                         "Version: " .. pkgversion )
+   content = string.gsub (content,  
+                         "Date: %d%d%d%d%/%d%d%/%d%d",
+                         "Date: " .. tagdate )
+ return content
+  end
+ return content
+ end
+ 
 -- ctan setup
 docfiles = {"sources/scontents.pdf","source/scontents.dtx"}
 textfiles= {"sources/README.md"}
