44e4450930e31a22976ab46893122089657a5f08
Pablo González L <pablgonz@yahoo.com>
2019-09-22 12:51:34 -0400

    The new version is ready: * Changes in dates. * Adjustments to build.lua * Adjustments to the tests. * Cosmetic changes in documentation.

--- build.lua
+++ build.lua
@@ -1,6 +1,6 @@
 -- Build script for scontents
 pkgversion   = "1.3"
-pkgdate      = "2019/09/23"
+pkgdate      = "2019/09/24"
 
 module       = "scontents"
 ctanpkg      = "scontents"
@@ -11,20 +11,20 @@ tagfiles     = {"sources/README.md","sources/scontents.sty","sources/scontents.d
 function update_tag (file,content,tagname,tagdate)
  tagdate = string.gsub (pkgdate,"-", "/")
  if string.match (file, "scontents.sty" ) then
-  content = string.gsub (content,
+  content = string.gsub (content,  
                          "\\ProvidesExplPackage{(.-)}{.-}{.-}",
                          "\\ProvidesExplPackage{%1}{".. tagdate.."}{"..pkgversion .."}")
-  return content
+  return content                         
  elseif string.match (file, "scontents.dtx") then
-  content = string.gsub (content,
+  content = string.gsub (content,  
                          "\\ProvidesExplPackage{(.-)}{.-}{.-}",
                          "\\ProvidesExplPackage{%1}{".. tagdate.."}{"..pkgversion .. "}")
-  return content
+  return content 
  elseif string.match (file, "README.md") then
-   content = string.gsub (content,
-                         "Version: %d%.%d.%s-",
-                         "Version: " .. pkgversion )
-   content = string.gsub (content,
+   content = string.gsub (content,  
+                         "Version: %d%.%d.%s- ",
+                         "Version: " .. pkgversion  )
+   content = string.gsub (content,  
                          "Date: %d%d%d%d%/%d%d%/%d%d",
                          "Date: " .. tagdate )
  return content
