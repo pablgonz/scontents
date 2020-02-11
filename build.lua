22807bafb28a5647ea07c0c1b94f95d9b56d8bfb
Pablo González L <pablgonz@yahoo.com>
2020-02-11 19:36:02 -0300

    Mumble

--- build.lua
+++ build.lua
@@ -227,7 +227,7 @@ if options["target"] == "release" then
       print('** Checking for pending commits')
     else
       print('** There are pending commits, running git push')
-      --os.execute("git push")
+      os.execute("git push")
       print('** You must run l3build release again')
       os.exit()
     end
@@ -244,9 +244,9 @@ if options["target"] == "release" then
     end
     print('** Everything is correct, we record changes in git by running')
     print("git tag -a v"..pkgversion.." -m 'Release v"..pkgversion.." "..pkgdate.."' && git push --tags")
-    --os.execute("git tag -a v"..pkgversion.." -m 'Release v"..pkgversion.."' && git push --tags")
+    os.execute("git tag -a v"..pkgversion.." -m 'Release v"..pkgversion.." "..pkgdate.."' && git push --tags")
     print('** We created the compressed package to send to ctan')
-    --os.execute("l3build ctan && l3build upload -F ctan.ann --debug")
+    os.execute("l3build ctan && l3build upload -F ctan.ann --debug")
     print('** We must add the changes for this version in ctan.ann file')
     print('** And finally (if everything is ok) execute manually (without --debug):')
     print('l3build upload -F ctan.ann')
