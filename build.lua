72e2f127252e147a91003850b19963173ad7c81d
pablo <pablgonz@yahoo.com>
2025-05-05 16:16:18 -0400

    Changes 2025-05-05: * Restructuring code for documentation and implementation. * Add new keys for `\typestored` and `\meaningsc`. * Check the version of `expl3` in plain TeX and ConTeXt.

--- build.lua
+++ build.lua
@@ -22,7 +22,7 @@
 -- General package identification
 module     = "scontents"
 pkgversion = "2.4"
-pkgdate    = "2025-04-29"
+pkgdate    = "2025-05-05"
 
 -- Configuration of files for build and installation
 maindir       = "."
